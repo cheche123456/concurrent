818de9aee20faaf4f1efded8f7455d26f1700260
cheche <604039142@qq.com>
2019-05-04 10:56:34 +0800

    che的修改

---
+++ concurrent1/src/com/cql/test/Test.java
@@ -0,0 +1,19 @@
+package com.cql.test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Test {
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            new Thread(()->{
+                countDownLatch.countDown();
+            }).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
